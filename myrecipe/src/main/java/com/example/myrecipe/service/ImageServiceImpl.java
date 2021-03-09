9d8c11f751432735e5669023b1f3cc39f4caee14
krish <krish@DESKTOP-95JLB01>
2021-03-09 20:58:13 +0530

    Changes to persist image to H2 DB as byte array

--- myrecipe/src/main/java/com/example/myrecipe/service/ImageServiceImpl.java
+++ myrecipe/src/main/java/com/example/myrecipe/service/ImageServiceImpl.java
@@ -1,18 +1,46 @@
 package com.example.myrecipe.service;
 
+import java.io.IOException;
+
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.myrecipe.domain.Recipe;
+import com.example.myrecipe.repositories.RecipeRepository;
+
 
 
 @Service
 public class ImageServiceImpl implements ImageService {
+	
+	RecipeRepository recipeRepo;
 	Logger log = LoggerFactory.getLogger(getClass());
+	
+	public ImageServiceImpl(RecipeRepository recipeRepo) {
+		this.recipeRepo = recipeRepo;
+	}
+	
 	@Override
+	@Transactional
 	public void saveImageFile(Long recipeId, MultipartFile file) {
-		log.debug("Received image file");
+		try {
+			Recipe recipe = recipeRepo.findById(recipeId).get();
+			Byte[] objects = new Byte[file.getBytes().length];
+			int i = 0;
+			for(byte b : file.getBytes()) {
+				objects[i++] = b;
+			}
+			recipe.setImage(objects);
+			recipeRepo.save(recipe);
+		}
+		catch(IOException e) {
+			log.error("Error occured", e);
+			e.printStackTrace();
+		}
 
 	}
 
